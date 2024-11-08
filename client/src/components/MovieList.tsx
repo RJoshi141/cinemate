44446edef3c830c7b52b09140d965b20d365cc3c
Ritika Joshi <63815734+RJoshi141@users.noreply.github.com>
2024-11-07 17:41:31 -0800

    back end commit

--- client/src/components/MovieList.tsx
+++ client/src/components/MovieList.tsx
@@ -4,9 +4,10 @@ const MovieList: React.FC = () => {
   const [movies, setMovies] = useState<string>('');
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/movies')
+    fetch(`${process.env.REACT_APP_API_URL}/movies`)
       .then((response) => response.json())
-      .then((data) => setMovies(data.message));
+      .then((data) => setMovies(data.message)) // or use `data.results` depending on the API response
+      .catch((error) => console.error('Error fetching movies:', error));
   }, []);
 
   return <div>{movies}</div>;
